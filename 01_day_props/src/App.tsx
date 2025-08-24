df3d338388bc78cbde0ae398ee0027a7c96f6c6d
Dipendu Jana <dipendujanaofficial5@gmail.com>
2025-08-24 11:26:07 +0530

    feat : add button props

--- 01_day_props/src/App.tsx
+++ 01_day_props/src/App.tsx
@@ -7,9 +7,9 @@ function App() {
   return (
     <div>
       <h1 className="text-3xl text-red-500 font-bold underline">Hello world!</h1>
-      <div className="grid grid-cols-2 gap-4">
-        <Card name="SuperMan" image="https://cdn.pixabay.com/photo/2024/01/11/16/29/leaves-8502135_1280.jpg" className="border-2 border-red-500" />
-        <Card name="Batman" image="https://cdn.pixabay.com/photo/2025/08/03/15/10/cat-9752539_1280.jpg" className="border-2 border-blue-500" />
+      <div className="grid  md:grid-cols-2 gap-4">
+        <Card name="Lips" image="https://cdn.pixabay.com/photo/2024/01/11/16/29/leaves-8502135_1280.jpg" className="border-2 border-red-500" />
+        <Card name="Cat" btnText="Follow me" image="https://cdn.pixabay.com/photo/2025/08/03/15/10/cat-9752539_1280.jpg" className="border-2 border-blue-500" />
       </div>
     </div>
   )
--- 01_day_props/src/components/card.tsx
+++ 01_day_props/src/components/card.tsx
@@ -2,12 +2,13 @@ import { cn } from "../lib/utils";
 
 
 
-export default function Card({name = 'Batman', image, className}: {name?: string, image: string, className: string}) {
+export default function Card({name = 'Batman', image, className, btnText = 'Click me'}: {name?: string, image: string, className: string, btnText?: string}) {
   return (
-    <div className={cn("bg-blue-500 grid gap-4 mt-10 rounded-md", className)}>
-      <div className="grid bg-red-500">
-        <h1 className="text-2xl font-bold text-center text-white p-2">{name}</h1>
+    <div className={cn("grid gap-4 mt-10 rounded-md", className)}>
+      <div className="grid">
+        <h1 className="text-2xl font-bold text-center text-white p-2 bg-red-500">{name}</h1>
         <img src={image} alt="placeholder" className="w-full h-[300px] object-cover overflow-hidden" />
+        <button className="bg-orange-500 text-white hover:bg-orange-400 cursor-pointer p-2 rounded-md w-1/2 mx-auto my-2">{btnText}</button>
       </div>
     </div>
   )
